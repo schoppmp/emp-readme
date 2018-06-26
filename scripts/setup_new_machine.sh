631c9a787bc3a5b0e0743e05bfef89a088c3cf50
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-06-26 13:11:41 +0200

    Fix setup script

--- scripts/setup_new_machine.sh
+++ scripts/setup_new_machine.sh
@@ -1,8 +1,9 @@
 git clone https://github.com/schoppmp/emp-readme.git
+cd emp-readme
 git checkout compat
-bash ./emp-readme/scripts/install_packages.sh
-bash ./emp-readme/scripts/install_relic.sh
-bash ./emp-readme/scripts/install_emp-tool.sh
-bash ./emp-readme/scripts/install_emp-ot.sh
-bash ./emp-readme/scripts/install_emp-m2pc.sh
-bash ./emp-readme/scripts/install_emp-sh2pc.sh
+bash ./scripts/install_packages.sh
+bash ./scripts/install_relic.sh
+bash ./scripts/install_emp-tool.sh
+bash ./scripts/install_emp-ot.sh
+bash ./scripts/install_emp-m2pc.sh
+bash ./scripts/install_emp-sh2pc.sh
